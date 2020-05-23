bdfa1bd43e150e6f9e025cd7d5815a394405ddcc
Jérémie Vézina <jeremie.vezina@hotmail.com>
2020-05-23 12:35:39 -0400

    first

---
+++ Base64Tests/Base64Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using EncodeurBase64;
+using System.Text;
+
+namespace Base64Tests
+{
+	[TestClass]
+	public class Base64Tests
+	{
+		[TestMethod]
+		public void EncodeStringNoPadding()
+		{
+			var bytes = Encoding.ASCII.GetBytes("Some s");
+			Base64.Encode(bytes).Should().Be(Convert.ToBase64String(bytes));
+		}
+
+		[TestMethod]
+		public void EncodeStringTwoPadding()
+		{
+			var bytes = Encoding.ASCII.GetBytes("1234");
+			Base64.Encode(bytes).Should().Be(Convert.ToBase64String(bytes));
+		}
+
+		[TestMethod]
+		public void EncodeStringOnePadding()
+		{
+			var bytes = Encoding.ASCII.GetBytes("abcde");
+			Base64.Encode(bytes).Should().Be(Convert.ToBase64String(bytes));
+		}
+
+		[TestMethod]
+		public void EncodeBytes()
+		{
+			var bytes = new byte[] { 106, 119, 196 };
+			Base64.Encode(bytes).Should().Be("anfE");
+		}
+	}
+}
---
+++ EncodeurBase64/Base64.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncodeurBase64
+{
+	public static class Base64
+	{
+		private const int NB_BITS_PER_CHAR = 6;
+
+		private static char[] charactersTable = new char[]
+		{
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '+'
+		};
+		public static String Encode(byte[] source)
+		{
+			int mod = source.Length % 3;
+			var nbPadding = mod == 2 ? 1 : mod % 3 == 1 ? 2 : 0;
+			StringBuilder result = new StringBuilder();
+
+			var listBits = ConvertByteArrayToBoolList(source);
+			for (int i = 0; i < nbPadding; i++)
+				listBits.AddRange(ConvertByteToBoolArray(Byte.MinValue));
+
+			var bits = listBits.ToArray();
+
+			for (int i = 0; i < bits.Length - (nbPadding * NB_BITS_PER_CHAR); i += NB_BITS_PER_CHAR)
+			{
+				int charIndex = 0;
+				for (int k = 0; k < NB_BITS_PER_CHAR; k++)
+				{
+					if (bits[i + k])
+						charIndex += 1 << (NB_BITS_PER_CHAR - 1 - k);
+				}
+				result.Append(charactersTable[charIndex]);
+			}
+
+			for (int i = 0; i < nbPadding; i++)
+				result.Append("=");
+
+			return result.ToString();
+		}
+
+		private static bool[] ConvertByteToBoolArray(byte b)
+		{
+			bool[] result = new bool[8];
+
+			for (int i = 0; i < 8; i++)
+				result[i] = (b & (1 << i)) == 0 ? false : true;
+
+			Array.Reverse(result);
+
+			return result;
+		}
+
+		private static List<bool> ConvertByteArrayToBoolList(byte[] b)
+		{
+			List<bool> lst = new List<bool>();
+			foreach (var by in b)
+				lst.AddRange(ConvertByteToBoolArray(by));
+
+			return lst;
+		}
+
+	}
+}
